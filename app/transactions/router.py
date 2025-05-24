068fd5a7901424fb61a336cac3132b8e862e90a5
kiranetic <kirankumar.rathod@outlook.com>
2025-05-24 13:24:56 +0000

    User authentication for transactions

--- app/transactions/router.py
+++ app/transactions/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 from app.db.session import get_db
+from app.auth.dependencies import get_current_user
 from app.transactions.schemas import TransactionCreate, TransactionResponse
 from app.transactions.crud import (
     create_transaction,
@@ -15,15 +16,15 @@ router = APIRouter(
 )
 
 @router.post("/", response_model=TransactionResponse)
-def create_transaction_endpoint(transaction_data: TransactionCreate, db: Session = Depends(get_db), user_id: int = 1):
-    return create_transaction(db, transaction_data, user_id = 1)
+def create_transaction_endpoint(transaction_data: TransactionCreate, db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
+    return create_transaction(db, transaction_data, current_user)
 
 @router.get("/", response_model=List[TransactionResponse])
-def get_all_transactions_endpoint(db: Session = Depends(get_db), user_id: int = 1):
-    return get_all_transactions(db=db, user_id=user_id)
+def get_all_transactions_endpoint(db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
+    return get_all_transactions(db, current_user)
 
 @router.get("/{transaction_id}", response_model=TransactionResponse)
-def get_single_transaction_endpoint(transaction_id: int, db: Session = Depends(get_db), user_id: int = 1):
+def get_single_transaction_endpoint(transaction_id: int, db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
     transaction = get_transaction_by_id(db, transaction_id)
     if not transaction:
         raise HTTPException(status_code=400, detail="Transaction not found")
